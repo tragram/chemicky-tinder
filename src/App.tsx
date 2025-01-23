7dca4db134528db45a557ff2eed4f50917bee64b
Dominik Hodan <domho108@gmail.com>
2025-01-23 16:18:20 +0100

    add a "match" website for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
 import DatingPage from "@/pages/dating";
+import MatchScreen from "./components/MatchScreen";
+
+import profiles from '@/dating_profiles.yaml';
 
 function App() {
   return (
     <Routes>
       <Route element={<DatingPage />} path="/kachekran-tinder/" />
+      <Route element={<MatchScreen userName={"Nadějný Chemik"} profile={profiles[0]} onContinue={undefined} />} path="/kachekran-tinder/match" />
     </Routes>
   );
 }
--- src/pages/dating.tsx
+++ src/pages/dating.tsx
@@ -3,7 +3,6 @@ import data from '@/dating_profiles.yaml';
 import TinderStack from "@/components/TinderStack";
 
 export default function DatingPage() {
-  console.log(data)
   return (
     <DefaultLayout>
       <div className="flex w-screen h-screen items-center justify-center p-8">

ef16062e4ffd1f4759fc0a6a0248f826e76eb1e9
Dominik Hodan <domho108@gmail.com>
2025-01-23 16:56:00 +0100

    use hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import DatingPage from "@/pages/dating";
 import MatchScreen from "./components/MatchScreen";
 
 import profiles from '@/dating_profiles.yaml';
+import { Provider } from "./provider";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<DatingPage />} path="/" />
-      <Route element={<MatchScreen userName={"Nadějný Chemik"} profile={profiles[0]} onContinue={undefined} />} path="/match" />
-    </Routes>
+    <Router>
+      <Provider>
+        <Routes>
+          <Route element={<DatingPage />} path="/" />
+          <Route element={<MatchScreen userName={"Nadějný Chemik"} profile={profiles[0]} onContinue={undefined} />} path="/match" />
+        </Routes>
+      </Provider>
+    </Router >
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router basename="/kachekran-tinder">
-      <Provider>
-        <App />
-      </Provider>
-    </Router>
+    <App />
   </React.StrictMode>,
 );

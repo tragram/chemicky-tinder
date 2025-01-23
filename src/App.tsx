68820b9677ac6e06be5e518573638e9599601eb5
Dominik Hodan <domho108@gmail.com>
2025-01-23 16:44:02 +0100

    try to fix routing on gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import DatingPage from "@/pages/dating";
 import MatchScreen from "./components/MatchScreen";
@@ -8,8 +8,8 @@ import profiles from '@/dating_profiles.yaml';
 function App() {
   return (
     <Routes>
-      <Route element={<DatingPage />} path="/kachekran-tinder/" />
-      <Route element={<MatchScreen userName={"Nadějný Chemik"} profile={profiles[0]} onContinue={undefined} />} path="/kachekran-tinder/match" />
+      <Route element={<DatingPage />} path="/" />
+      <Route element={<MatchScreen userName={"Nadějný Chemik"} profile={profiles[0]} onContinue={undefined} />} path="/match" />
     </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
@@ -8,10 +8,10 @@ import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router basename="/kachekran-tinder">
       <Provider>
         <App />
       </Provider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 );

e568d5e7177df22dde1c8f2bb986949ff4ee49f0
Dominik Hodan <domho108@gmail.com>
2025-02-13 12:43:59 +0100

    change base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import yaml from "unplugin-yaml/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/chemicky-tinder/",
+  base: "/",
   plugins: [react(), tsconfigPaths(), yaml()],
   build: {
     outDir: "dist",
